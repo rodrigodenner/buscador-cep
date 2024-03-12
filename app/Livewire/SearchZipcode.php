bb7528c74543074c29686d13d0e696e70be9118b
rodrigodenner <rodrigodenner.art@gmail.com>
2024-03-12 00:39:27 -0300

    First commit

---
+++ app/Livewire/SearchZipcode.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class SearchZipcode extends Component
+{
+    public function render()
+    {
+        return view('livewire.search-zipcode');
+    }
+}
