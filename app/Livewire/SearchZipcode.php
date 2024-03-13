8b8893d371cb533d6a803b6ff805b8eac864f73d
rodrigodenner <rodrigodenner.art@gmail.com>
2024-03-13 01:08:15 -0300

    fix: update SeachZipcode.php and seach-zipcode coponent

--- app/Livewire/SearchZipcode.php
+++ app/Livewire/SearchZipcode.php
@@ -2,27 +2,77 @@
 
 namespace App\Livewire;
 
+use App\Models\Address;
 use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
 class SearchZipcode extends Component
 {
-  public $zipcode = '';
-  public $logradouro = '';
-  public $complemento = '';
-  public $bairro = '';
-  public $localidade = '';
-  public $uf = '';
+    public $zipcode = '';
+    public $logradouro = '';
+    public $bairro = '';
+    public $localidade = '';
+    public $uf = '';
+    protected $rules = [
+      'zipcode' => 'required',
+      'logradouro' => 'required',
+      'bairro' => 'required',
+      'localidade' => 'required',
+      'uf' => 'required|max:2',
+  ];
 
-  public function updatedZipcode($value)
-  {
-    dd($value);
-  }
-  public function mount()
-  {
-      // $response = Http::withoutVerifying()->get("https://opencep.com/v1/15050305")->json();
-      // dd($response);
-  }
+    protected $messages = [
+        'zipcode.required' => 'O campo CEP é obrigatório',
+        'logradouro.required' => 'O campo Logradouro é obrigatório',
+        'bairro.required' => 'O campo Bairro é obrigatório',
+        'localidade.required' => 'O campo Localidade é obrigatório',
+        'uf.required' => 'O campo UF é obrigatório',
+        'uf.max' => 'O campo UF deve ter no máximo 2 caracteres',
+    ];
+
+
+    public function updatedZipcode($value)
+    {
+        $response = Http::withoutVerifying()->get("https://opencep.com/v1/{$value}")->json();
+        if (isset($response['zipcode'])) {
+            $this->zipcode = $response['zipcode'];
+        }
+
+        if (isset($response['logradouro'])) {
+            $this->logradouro = $response['logradouro'];
+        }
+
+        if (isset($response['bairro'])) {
+            $this->bairro = $response['bairro'];
+        }
+
+        if (isset($response['localidade'])) {
+            $this->localidade = $response['localidade'];
+        }
+
+        if (isset($response['uf'])) {
+            $this->uf = $response['uf'];
+        }
+    }
+
+    public function save(Address $address)
+    {
+        $this->validate();
+        $address->updateOrCreate(
+            [
+            'zipcode' => $this->zipcode, //não vai cadastrar de novo
+          ],
+            [
+            'logradouro' => $this->logradouro, //pode cadastrar novamente
+            'bairro' => $this->bairro,
+            'localidade' => $this->localidade,
+            'uf' => $this->uf,
+          ]
+        );
+
+        $this->reset();
+    }
+
     public function render()
     {
         return view('livewire.search-zipcode');
--- resources/views/livewire/search-zipcode.blade.php
+++ resources/views/livewire/search-zipcode.blade.php
@@ -2,36 +2,37 @@
   <h1 class="text-2xl font-bold mb-4">Busca CEP</h1>
 
   <form>
-      <div class="mb-4">
-          <label for="cep" class="block text-sm font-medium text-gray-600">CEP</label>
-          <input wire:model.lazy="zipcode" type="text" id="cep" name="cep" class="mt-1 p-2 border w-full rounded-md">
-      </div>
+    <div class="mb-4">
+      <label for="cep" class="block text-sm font-medium text-gray-600">CEP</label>
+      <input wire:model.lazy="zipcode" type="text" id="cep" name="cep" class="mt-1 p-2 border w-full rounded-md">
+      @error('zipcode') <span class="text-red-500 bg-red-100">{{ $message }}</span> @enderror
+    </div>
 
-      <div class="mb-4">
-          <label for="logradouro" class="block text-sm font-medium text-gray-600">Logradouro</label>
-          <input wire:model="logradouro" type="text" id="logradouro" name="logradouro" class="mt-1 p-2 border w-full rounded-md">
-      </div>
+    <div class="mb-4">
+      <label for="logradouro" class="block text-sm font-medium text-gray-600">Logradouro</label>
+      <input wire:model="logradouro" type="text" id="logradouro" name="logradouro" class="mt-1 p-2 border w-full rounded-md">
+      @error('logradouro') <span class="text-red-500 bg-red-100">{{ $message }}</span> @enderror
+    </div>
 
-      <div class="mb-4">
-          <label for="complemento" class="block text-sm font-medium text-gray-600">Complemento</label>
-          <input wire:model="complemento" type="text" id="complemento" name="complemento" class="mt-1 p-2 border w-full rounded-md">
-      </div>
+    <div class="mb-4">
+      <label for="bairro" class="block text-sm font-medium text-gray-600">Bairro</label>
+      <input wire:model="bairro" type="text" id="bairro" name="bairro" class="mt-1 p-2 border w-full rounded-md">
+      @error('bairro') <span class="text-red-500 bg-red-100">{{ $message }}</span> @enderror
+    </div>
 
-      <div class="mb-4">
-          <label for="bairro" class="block text-sm font-medium text-gray-600">Bairro</label>
-          <input wire:model="bairro" type="text" id="bairro" name="bairro" class="mt-1 p-2 border w-full rounded-md">
-      </div>
+    <div class="mb-4">
+      <label for="localidade" class="block text-sm font-medium text-gray-600">Localidade</label>
+      <input wire:model="localidade" type="text" id="localidade" name="localidade" class="mt-1 p-2 border w-full rounded-md">
+      @error('localidade') <span class="text-red-500 bg-red-100">{{ $message }}</span> @enderror
+    </div>
 
-      <div class="mb-4">
-          <label for="localidade" class="block text-sm font-medium text-gray-600">Localidade</label>
-          <input wire:model="localidade" type="text" id="localidade" name="localidade" class="mt-1 p-2 border w-full rounded-md">
-      </div>
+    <div class="mb-4">
+      <label for="uf" class="block text-sm font-medium text-gray-600">UF</label>
+      <input wire:model="uf" type="text" id="uf" name="uf" class="mt-1 p-2 border w-full rounded-md">
+      @error('uf') <span class="text-red-500 bg-red-100">{{ $message }}</span> @enderror
+    </div>
 
-      <div class="mb-4">
-          <label for="uf" class="block text-sm font-medium text-gray-600">UF</label>
-          <input wire:model="uf" type="text" id="uf" name="uf" class="mt-1 p-2 border w-full rounded-md">
-      </div>
-
-      <button type="buttom" class="bg-blue-500 text-white p-2 rounded-md">Salvar</button>
+    <button type="button" class="bg-blue-500 text-white p-2 rounded-md" wire:click='save'>Salvar</button>
   </form>
 </div>
+
